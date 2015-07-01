8b73dc629e5bdefe0d3272288e9e0c6d48da7755
Nickolas Marouchos <nickolas.marouchos@wooga.net>
2015-07-01 12:12:24 +0200

    First pass on Combat Controller

---
+++ Assets/Scripts/CombatController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatController : MonoBehaviour {
+	
+	public float _combatInterval;
+	private float m_currentInterval;
+
+	private bool m_withinInterval = false;
+
+	private Time m_enabledTime;
+
+	// Use this for initialization
+	void Start () {
+		ResetInterval();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RunInterval();
+	}
+
+	private void ResetInterval()
+	{
+		Debug.Log ("ResetInterval()");
+		m_currentInterval = _combatInterval;
+	}
+
+	private void RunInterval()
+	{
+		m_currentInterval -= Time.deltaTime;
+		if (m_currentInterval <= 0f)
+		{
+			ResetInterval();
+		}
+	}
+
+}
